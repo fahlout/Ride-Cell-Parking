76169c2acc3ae0d4cc10c388d029e34aa2572fb6
Niklas Fahl <niklas.fahl@me.com>
2017-01-15 14:54:12 -0600

    - Added information to my car tab

--- RideCellParking/RideCellParking/Controllers/MyCarTableViewController.swift
+++ RideCellParking/RideCellParking/Controllers/MyCarTableViewController.swift
@@ -26,67 +26,64 @@ class MyCarTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        registerCells()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func registerCells() {
+        tableView.register(UINib(nibName: "TwoLabelsTableViewCell", bundle: nil), forCellReuseIdentifier: "TwoLabelsTableViewCell")
+    }
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 4
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+        if indexPath.row == 0 {
+            // Name
+            let cell = tableView.dequeueReusableCell(withIdentifier: "TwoLabelsTableViewCell", for: indexPath) as! TwoLabelsTableViewCell
+            
+            // Configure the cell...
+            cell.setContent(for: "Name", detail: "Niklas Fahl")
+            
+            return cell
+        } else if indexPath.row == 1 {
+            // Car
+            let cell = tableView.dequeueReusableCell(withIdentifier: "TwoLabelsTableViewCell", for: indexPath) as! TwoLabelsTableViewCell
+            
+            // Configure the cell...
+            cell.setContent(for: "Car", detail: "Ford Fiesta ST")
+            
+            return cell
+        } else if indexPath.row == 2 {
+            // Location
+            let cell = tableView.dequeueReusableCell(withIdentifier: "TwoLabelsTableViewCell", for: indexPath) as! TwoLabelsTableViewCell
+            
+            // Configure the cell...
+            cell.setContent(for: "Location", detail: "San Francisco, CA")
+            
+            return cell
+        } else if indexPath.row == 3 {
+            // Credit Card
+            let cell = tableView.dequeueReusableCell(withIdentifier: "TwoLabelsTableViewCell", for: indexPath) as! TwoLabelsTableViewCell
+            
+            // Configure the cell...
+            cell.setContent(for: "Credit Card", detail: "*1234")
+            
+            return cell
+        } else {
+            return UITableViewCell()
+        }
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 }
